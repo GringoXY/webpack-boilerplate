af71058f9a5a97e4cab012cb49b7317c750a32f0
GringoXY <przemyslawkowalski01pk@gmail.com>
2020-11-23 18:30:18 +0100

    Add properties to webpack development

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,9 +5,11 @@ const config = require('./webpack.config');
 module.exports = merge(config, {
   mode: 'development',
   devtool: 'inline-source-map',
+  target: 'web',
 
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
+    watchContentBase: true,
     compress: true,
     open: true,
     port: 9000,
